406be7c256002dae0a9417b020ad091935c8b38d
Leo Famulari <leo@famulari.name>
2016-03-05 02:33:09 -0500

    gnu: Add gnome-maps.
    * gnu/packages/maps.scm: New file.
    * gnu-system.am (GNU_SYSTEM_MODULES): Add it.

---
+++ gnu/packages/maps.scm
@@ -0,0 +1,83 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2016 Leo Famulari <leo@famulari.name>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages maps)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix download)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (gnu packages glib) ; intltool
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages xml))
+
+(define-public gnome-maps
+  (package
+    (name "gnome-maps")
+    (version "3.18.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnome/sources/" name "/"
+                                  (version-major+minor version) "/"
+                                  name "-" version ".tar.xz"))
+              (sha256
+               (base32
+                "0y4jmh5hwskh2mnladh9hxp9k8as7crm8wwwiifvxsjjj9az2gv9"))))
+    (build-system glib-or-gtk-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after
+          'install 'wrap
+          (lambda* (#:key inputs outputs #:allow-other-keys)
+            (let ((out (assoc-ref outputs "out"))
+                  (gi-typelib-path (getenv "GI_TYPELIB_PATH"))
+                  (goa-path (string-append
+                              (assoc-ref inputs "gnome-online-accounts")
+                              "/lib")))
+              (wrap-program (string-append out "/bin/gnome-maps")
+               `("GI_TYPELIB_PATH" ":" prefix (,gi-typelib-path))
+               `("LD_LIBRARY_PATH" ":" prefix (,goa-path)))
+              #t))))))
+    (native-inputs
+     `(("gobject-introspection" ,gobject-introspection)
+       ("intltool" ,intltool)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("folks" ,folks)
+       ("libchamplain" ,libchamplain)
+       ("libgee" ,libgee)
+       ("libxml2" ,libxml2)
+       ("geoclue" ,geoclue)
+       ("geocode-glib" ,geocode-glib)
+       ("gfbgraph" ,gfbgraph)
+       ("gjs" ,gjs)
+       ("glib" ,glib)
+       ("gnome-online-accounts" ,gnome-online-accounts) ; necessary?
+       ("rest" ,rest)
+       ("webkitgtk" ,webkitgtk))) ; necessary?
+    (propagated-inputs
+     `(("gtk+3" ,gtk+))) ; necessary?
+    (synopsis "Graphical map viewer")
+    (description "GNOME Maps is a graphical map viewer.  It uses map data from
+the OpenStreetMap project.")
+    (home-page "https://wiki.gnome.org/Apps/Maps")
+    (license gpl2+)))
