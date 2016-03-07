b9adfdb58598753332da0323d394d75cdbe5e7dc
Leo Famulari <leo@famulari.name>
2016-03-07 02:17:22 -0500

    gnu: Add gfbgraph.
    * gnu/packages/gnome.scm (gfbgraph): New variable.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5326,3 +5326,36 @@ create metacontacts.  It's written in Vala, which generates C code when
 compiled.")
     (home-page "https://wiki.gnome.org/Projects/Folks")
     (license license:lgpl2.1+)))
+
+(define-public gfbgraph
+  (package
+    (name "gfbgraph")
+    (version "0.2.3")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://gnome/sources/" name "/"
+                    (version-major+minor version) "/"
+                    name "-" version ".tar.xz"))
+              (sha256
+               (base32
+                "1dp0v8ia35fxs9yhnqpxj3ir5lh018jlbiwifjfn8ayy7h47j4fs" ; 0.2.3
+                ))))
+    (build-system glib-or-gtk-build-system)
+    (arguments
+     `(#:tests? #f ; http://www.linuxfromscratch.org/blfs/view/stable-systemd/gnome/gfbgraph.html
+       #:configure-flags '("--disable-gtk-doc" ; --enable-gtk-doc fails even with gtk-doc as native-input
+                           "--disable-static"
+                           "--enable-introspection")))
+    (native-inputs
+     `(("pkg-config" ,pkg-config) ; necessary?
+       ("gobject-introspection" ,gobject-introspection)))
+    (inputs
+     `(("json-glib" ,json-glib)
+       ("gnome-online-accounts" ,gnome-online-accounts)
+       ("rest" ,rest)))
+    (synopsis "GLib/GObject wrapper for the Facebook API")
+    (description "This library allows you to use the Facebook API from
+GLib/GObject code.")
+    (home-page "https://wiki.gnome.org/Projects/GFBGraph")
+    (license license:lgpl2.1+)))
